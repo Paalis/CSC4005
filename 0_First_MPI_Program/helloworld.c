a6fbd3d18f975842307b8d17dc499236f0651eee
TomSGao <gaoshen1004@gmail.com>
2017-09-23 14:51:38 +0800

    Send and receive

--- 0_First_MPI_Program/helloworld.c
+++ 0_First_MPI_Program/helloworld.c
@@ -3,22 +3,22 @@
 
 int main(int argc, char* argv[])
 {
-  int procid, numprocs;
+  int ierr, procid, numprocs;
 
   // init
-  MPI_Init(&argc, &argv);
+  ierr = MPI_Init(&argc, &argv);
   
   // find out process rank
-  MPI_Comm_rank(MPI_COMM_WORLD, &procid);
+  ierr = MPI_Comm_rank(MPI_COMM_WORLD, &procid);
 
   // No. of Process
-  MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+  ierr = MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
 
   // let each process print something;
   printf("Hello world from Process #%d out of %d!\n", procid, numprocs);
   
   // shut down MPI
-  MPI_Finalize();
+  ierr = MPI_Finalize();
 
-  return 0;
+  return ierr;
 }
